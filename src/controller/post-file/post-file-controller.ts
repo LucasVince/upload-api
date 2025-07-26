6fa42a68fe0f41479e176998774e72519902ed74
LucasVince <lucashenriquevince1714@gmail.com>
2025-07-25 22:06:23 -0300

    feat: added security verification, and resize with sharp

--- src/controller/post-file/post-file-controller.ts
+++ src/controller/post-file/post-file-controller.ts
@@ -2,6 +2,9 @@ import { badRequest, ok, serverError } from "../helpers";
 import { HttpRequest, HttpResponse, iController } from "../protocols";
 import { logger } from "../../utils/logger";
 import { FileRequest } from "../protocols";
+import sharp from "sharp";
+import fs from "fs";
+import path from "path";
 
 export class postFileController implements iController {
     async handle(
@@ -14,8 +17,29 @@ export class postFileController implements iController {
                 return badRequest("no file sent");
             }
 
+            if (!file.mimetype.startsWith("image/")) {
+                fs.unlinkSync(file.path);
+                return badRequest("file sent was not and image");
+            }
+
+            const outputPath = path.resolve(
+                file.destination,
+                `resized ${file.filename}`
+            );
+
+            const image = sharp(file.path);
+            const metadata = await image.metadata();
+
+            if (metadata.width && metadata.width > 1000) {
+                await image.resize(1000).toFile(outputPath);
+                fs.unlinkSync(file.path);
+            } else if (metadata.height && metadata.height > 1000) {
+                await image.resize({ height: 1000 }).toFile(outputPath);
+                fs.unlinkSync(file.path);
+            }
+
             logger.info(
-                `File Sent successfully: ${file.originalname} (${file.size} bytes)`
+                `File Sent, and processed successfully: ${file.filename} (${file.size} bytes)`
             );
 
             return ok({
